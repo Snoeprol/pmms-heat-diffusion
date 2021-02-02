400a1da18d1b6cc9917a3ccfb3372ba1170db669
daanvinken <vinkendaan@gmail.com>
2021-02-02 16:37:31 +0100

    working on compute results but not working yet

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <input.h>
 #include <time.h>
-
+#include <output.h>
 
 double rtime;
 struct timeval start;
@@ -30,6 +30,7 @@ void do_compute(const struct parameters *p, struct results *r)
     double next[N][M];
     double current[N][M];
     double cond[N][M];
+    double **output_t;
 
     /* Fill next and conductivity as 2D matrix */
     for (size_t i = 0; i < N; i++)
@@ -42,7 +43,8 @@ void do_compute(const struct parameters *p, struct results *r)
     }
 
     /* Get start time */
-    if (gettimeofday(&start, 0) != 0) {
+    if (gettimeofday(&start, 0) != 0)
+    {
         fprintf(stderr, "could not do timing\n");
         exit(1);
     }
@@ -80,13 +82,57 @@ void do_compute(const struct parameters *p, struct results *r)
     }
 
     /* Get end time and print the duration */
-    if (gettimeofday(&end, 0) != 0) {
+    if (gettimeofday(&end, 0) != 0)
+    {
         fprintf(stderr, "could not do timing\n");
         exit(1);
     }
     rtime = (end.tv_sec + (end.tv_usec / 1000000.0)) -
-                (start.tv_sec + (start.tv_usec / 1000000.0));
+            (start.tv_sec + (start.tv_usec / 1000000.0));
     fprintf(stderr, "\n Simulation took %.3f seconds\n", rtime);
 
+    printf("Value in next is: %f\n", &current[4][4]);
 
+    compute_results(&p, &r, 0, (double*) current, rtime);
 }
+
+void compute_results(const struct parameters *p, struct results *r, int k, double **t_array, double rtime) {
+    r->niter = k;
+    int M = p->M;
+    int N = p->N;
+    double tmin = __INT_MAX__;
+    double tmax = -(__INT_MAX__);
+    double t_tot = 0;
+    double max_diff = 0;
+
+
+
+    printf("Result function received: %f\n", &t_array[4][4]);
+    // for (int i = 0; i < N; i++)
+    // {
+    //     for (int j = 1; j < M - 1; j++)
+    //     {
+    //         printf("%f\n", &t_array[i][j]);
+    //         // t_tot += t_array[i][j];
+    //         // if (t_array[i][j] > tmax)
+    //         //     tmax = t_array[i][j];
+    //         // if (t_array[i][j] < tmin)
+    //         //     tmin = t_array[i][j];
+    //         // for (int k = 0; i < N; i++)
+    //         // {
+    //         //     for (int l = 1; j < M - 1; j++)
+    //         //     {
+    //         //         if (abs(t_array[i][j] - t_array[k][l]) > max_diff)
+    //         //             max_diff = abs(t_array[i][j] - t_array[k][l]);
+    //         //     }
+    //         // }
+    //     }
+    // }
+    // r->time = rtime;
+    // r->niter = k;
+    // r->tmin = tmin;
+    // r->tmax = tmax;
+    // r->tavg = t_tot / N * M;
+    // r->maxdiff = max_diff;
+}
+
