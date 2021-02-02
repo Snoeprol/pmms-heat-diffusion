cd68faa230ae9ad0ab8a06452da386cff6e2b535
daanvinken <vinkendaan@gmail.com>
2021-02-02 14:51:04 +0100

    Added timing and time for loop

--- assignment_1/heat_seq/compute.c
+++ assignment_1/heat_seq/compute.c
@@ -4,25 +4,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <input.h>
+#include <time.h>
 
-    // assume cond is a 2-D array filled with conductivities of the lattice
-    // assume next is a 2-D array filled with heats at the next iteration
-    // assume current is a 2-D array filled with current heat configuration
-    // assume bound is the boundary value array (size (N, 2)) bound[x][0] is bottom
 
-    // size_t M = p->M;
-    // printf("--> %ld\n", M);
+double rtime;
+struct timeval start;
+struct timeval end;
 
-    // printf("%f ", next[(i+1)*j] );
-    // printf("hey\n");
+// assume cond is a 2-D array filled with conductivities of the lattice
+// assume next is a 2-D array filled with heats at the next iteration
+// assume current is a 2-D array filled with current heat configuration
+// assume bound is the boundary value array (size (N, 2)) bound[x][0] is bottom
 
-/* Define weak strong smth ??? */
+/* Define weak strong influences */
 const double weak_inf = 1 / 4 * (sqrt(2) + 1);
 const double strong_inf = sqrt(2) / 4 * (sqrt(2) + 1);
 
-
-
-void do_compute(const struct parameters* p, struct results *r)
+void do_compute(const struct parameters *p, struct results *r)
 {
     int M = p->M;
     int N = p->N;
@@ -41,49 +39,54 @@ void do_compute(const struct parameters* p, struct results *r)
             next[i][j] = p->tinit[(i + 1) * j];
             cond[i][j] = p->conductivity[(i + 1) * j];
         }
-
     }
 
+    /* Get start time */
+    if (gettimeofday(&start, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
 
     /* Start timesteps */
-
-    /* Copy next into current */
-    memcpy(current, next, sizeof(double) * num_bodies);
-
-    for (int i = 0; i < N; i++)
+    for (size_t step = 0; step < p->maxiter; ++step)
     {
-        for (int j = 1; j < M - 1; j++)
+
+        /* Copy next into current */
+        memcpy(current, next, sizeof(double) * num_bodies);
+
+        for (int i = 0; i < N; i++)
         {
-            /* Make sure were not on top or bottom boundary */
-            next[i][j] = cond[i][j] * current[i][j];
-            double inf = 1 - cond[i][j];
+            for (int j = 1; j < M - 1; j++)
+            {
+                /* Make sure were not on top or bottom boundary */
+                next[i][j] = cond[i][j] * current[i][j];
+                double inf = 1 - cond[i][j];
 
-            // printf("%f ", next[i][j]);
+                // printf("%f ", next[i][j]);
 
-            /* strong neighbors */
-            next[i][j] += strong_inf * inf * current[(i + 1) % N][j];
-            next[i][j] += strong_inf * inf * current[(i - 1 + N) % N][j];
-            next[i][j] += strong_inf * inf * current[i][j + 1];
-            next[i][j] += strong_inf * inf * current[i][j - 1];
-            /* weak neighbors */
-            next[i][j] += weak_inf * inf * current[(i - 1 + N) % N][j - 1];
-            next[i][j] += weak_inf * inf * current[(i + 1) % N][j - 1];
-            next[i][j] += weak_inf * inf * current[(i - 1 + N) % N][j + 1];
-            next[i][j] += weak_inf * inf * current[(i + 1) % N][j + 1];
+                /* strong neighbors */
+                next[i][j] += strong_inf * inf * current[(i + 1) % N][j];
+                next[i][j] += strong_inf * inf * current[(i - 1 + N) % N][j];
+                next[i][j] += strong_inf * inf * current[i][j + 1];
+                next[i][j] += strong_inf * inf * current[i][j - 1];
 
-
+                /* weak neighbors */
+                next[i][j] += weak_inf * inf * current[(i - 1 + N) % N][j - 1];
+                next[i][j] += weak_inf * inf * current[(i + 1) % N][j - 1];
+                next[i][j] += weak_inf * inf * current[(i - 1 + N) % N][j + 1];
+                next[i][j] += weak_inf * inf * current[(i + 1) % N][j + 1];
             }
         }
     }
 
+    /* Get end time and print the duration */
+    if (gettimeofday(&end, 0) != 0) {
+        fprintf(stderr, "could not do timing\n");
+        exit(1);
+    }
+    rtime = (end.tv_sec + (end.tv_usec / 1000000.0)) -
+                (start.tv_sec + (start.tv_usec / 1000000.0));
+    fprintf(stderr, "\n Simulation took %.3f seconds\n", rtime);
 
 
-/* final reporting */
-struct results {
-    size_t niter; /* effective number of iterations */
-    double tmin; /* minimum temperature in last state */
-    double tmax; /* maximum temperature in last state*/
-    double maxdiff; /* maximum difference during last update */
-    double tavg; /* average temperature */
-    double time; /* compute time in seconds */
-};
+}
