15575ee6cf942aa4aac43119bb89500a1e9803fb
snoeprol <mariovanrooij@hotmail.com>
2021-02-16 10:35:46 +0100

    fixed merge

--- assignment_2/mergesort/parallel/merge.c
+++ assignment_2/mergesort/parallel/merge.c
@@ -85,6 +85,7 @@ void msort(int *vector, long length, int num_threads)
             topDownSplitMerge(vector_work, 0, length, vector);
         };
     }
+    topDownMerge(v_work, 0, (int) l/2, l, v);
 }
 
 void print_v(int *v, long l)
