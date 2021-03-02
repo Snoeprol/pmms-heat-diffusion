aca6edc3d7453ddd062250e7b0761ddf044cb590
daanvinken <vinkendaan@gmail.com>
2021-03-02 15:19:34 +0100

    udpdate very close with heat thing

--- assignment_3/heat_pth/compute.c
+++ assignment_3/heat_pth/compute.c
@@ -8,19 +8,21 @@
 #include "compute.h"
 #include "ref1.c"
 
+ pthread_barrier_t barrier;
 
 /* Struct with parameters for each thread */
-
 typedef struct thread_param
 {
     int id;
     int row_start;
     int row_end;
     int maxiter;
+    size_t *iter;
 
     struct results *results;
     struct parameters *parameters;
     struct timeval *before;
+    struct timeval *after;
     double **src;
     double **dst;
     double **cond;
@@ -33,8 +35,9 @@ void *threadwork(void *param)
     static const double c_cdiag = 0.25 / (M_SQRT2 + 1.0);
     /* Get all values from parameter struct */
     thread_param *thread_parameters = (thread_param*) param;
-    size_t i, j;
+    size_t i, j, iter;
     struct timeval *before = thread_parameters->before;
+    struct timeval *after = thread_parameters->after;
     int maxiter = thread_parameters->parameters->maxiter;
     int h = thread_parameters->parameters->N + 2;
     int w = thread_parameters->parameters->M + 2;
@@ -44,45 +47,49 @@ void *threadwork(void *param)
     double (*restrict dst)[h][w] = thread_parameters->dst;
     int start_row = thread_parameters->row_start;
     int end_row = thread_parameters->row_end;
+    iter = thread_parameters->iter;
 
-    // for (iter = 1; iter <= p->maxiter; ++iter)
-    // {
-    //     /* swap source and destination */
-    //     { void *tmp = src; src = dst; dst = tmp; }
+    for (iter = 1; iter <= maxiter; ++iter)
+    {
+        /* swap source and destination */
+        { void *tmp = src; src = dst; dst = tmp; }
 
-    //     /* initialize halo on source */
-    //     do_copy(h, w, src);
+        /* initialize halo on source */
+        do_copy(h, w, src);
 
-    //     double maxdiff = 0.0;
-    //     /* compute */
-    //     for (i = 1; i < h - 1; ++i)
-    //         for (j = 1; j < w - 1; ++j)
-    //         {
-    //             double w = (*c)[i][j];
-    //             double restw = 1.0 - w;
+        double maxdiff = 0.0;
 
-    //             (*dst)[i][j] = w * (*src)[i][j] +
+        // pthread_barrier_wait(&barrier);
+        /* compute */
+        for (i = start_row; i < end_row; ++i)
+            for (j = 1; j < w - 1; ++j)
+            {
+                double w = (*c)[i][j];
+                double restw = 1.0 - w;
 
-    //                 ((*src)[i+1][j  ] + (*src)[i-1][j  ] +
-    //                  (*src)[i  ][j+1] + (*src)[i  ][j-1]) * (restw * c_cdir) +
+                (*dst)[i][j] = w * (*src)[i][j] +
 
-    //                 ((*src)[i-1][j-1] + (*src)[i-1][j+1] +
-    //                  (*src)[i+1][j-1] + (*src)[i+1][j+1]) * (restw * c_cdiag);
+                    ((*src)[i+1][j  ] + (*src)[i-1][j  ] +
+                     (*src)[i  ][j+1] + (*src)[i  ][j-1]) * (restw * c_cdir) +
 
-    //             double diff = fabs((*dst)[i][j] - (*src)[i][j]);
-    //             if (diff > maxdiff) maxdiff = diff;
-    //         }
-    //        r->maxdiff=maxdiff;
-    //     if(maxdiff<p->threshold){iter++;break;}
-    //     /* conditional reporting */
-    //     if (iter % p->period == 0) {
-    //        fill_report(p, r, h, w, dst, src, iter, &before);
-    //         if(p->printreports) report_results(p, r);
-    //     }
-    //     //#ifdef GEN_PICTURES
-    //     //do_draw(p, iter, h, w, src);
-    //     //#endif
-    // }
+                    ((*src)[i-1][j-1] + (*src)[i-1][j+1] +
+                     (*src)[i+1][j-1] + (*src)[i+1][j+1]) * (restw * c_cdiag);
+
+                double diff = fabs((*dst)[i][j] - (*src)[i][j]);
+                if (diff > maxdiff) maxdiff = diff;
+            }
+           thread_parameters->results->maxdiff=maxdiff;
+        if(maxdiff < thread_parameters->parameters->threshold){iter++;break;}
+        /* conditional reporting */
+        if (thread_parameters->id == 0) printf("thread %d iter = %d\n",thread_parameters->id, iter);
+        if (thread_parameters->id == 0 && iter % thread_parameters->parameters->period == 0) {
+           fill_report(thread_parameters->parameters, thread_parameters->results, h, w, dst, src, iter, thread_parameters->before);
+            if(thread_parameters->parameters->printreports) report_results(thread_parameters->parameters, thread_parameters->results);
+        }
+        //#ifdef GEN_PICTURES
+        //do_draw(p, iter, h, w, src);
+        //#endif
+    }
 }
 
 void do_compute(const struct parameters *p, struct results *r)
@@ -133,6 +140,7 @@ void do_compute(const struct parameters *p, struct results *r)
     /* Determine split size of grid over threads */
     int rows_per_threads[num_threads];
     int row_count = (h - 2) / num_threads;
+    for (int i = 0; i < num_threads; i++) { rows_per_threads[i] = row_count; }
     int rest = (h - 2) - (num_threads * row_count);
     int fill_rest_thread = 0;
     while (rest != 0)
@@ -154,6 +162,10 @@ void do_compute(const struct parameters *p, struct results *r)
     }
 
     pthread_t pthreads[num_threads];
+    pthread_barrierattr_t attr;
+    unsigned count;
+    int ret;
+    ret = pthread_barrier_init(&barrier, NULL, num_threads);
 
     /*Fill array of structs with all threads their parameters */
     thread_param thread_parameters[num_threads];
@@ -162,6 +174,7 @@ void do_compute(const struct parameters *p, struct results *r)
         thread_param *current_params = &thread_parameters[proc];
         current_params->id = proc;
         current_params->before = &before;
+        current_params->after = &after;
         current_params->results = r;
         current_params->parameters = p;
         current_params->row_end = threads_row_end[proc];
@@ -169,6 +182,7 @@ void do_compute(const struct parameters *p, struct results *r)
         current_params->cond = (double **)c;
         current_params->src = (double **)src;
         current_params->dst = (double **)dst;
+        current_params->iter = &iter;
 
         pthread_create(&pthreads[proc], NULL, threadwork, current_params);
     }
@@ -181,11 +195,12 @@ void do_compute(const struct parameters *p, struct results *r)
             fprintf(stderr, "Error joining thread\n");
             /* TODO returns status exit */
         }
-        printf("Thread %d finished!\n", proc);
     }
 
     /* report at end in all cases */
-    iter--;
+     iter = p->maxiter;
+    // iter--;
+    // printf("iter = %d\n", iter);
     fill_report(p, r, h, w, dst, src, iter, &before);
 
     free(c);
