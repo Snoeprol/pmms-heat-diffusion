1843a18f828d8acda9231efa4868ab0c64270550
daanvinken <vinkendaan@gmail.com>
2021-03-03 21:33:43 +0100

    workign on pipesort

--- assignment_3/pipesort/pipesort.c
+++ assignment_3/pipesort/pipesort.c
@@ -1,11 +1,58 @@
-#include <time.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <getopt.h>
 #include <ctype.h>
 #include <errno.h>
 
+typedef struct thread_parameters{
+    int *buffer;
+    // add some lock to the buffer
+} thread_parameters;
+
+int buffer_size = 5;
+pthread_t *threads;
+
+void test_function(int i) {
+    printf("Test function printing\n");
+}
+
+void generate_numbers(int *length) {
+    int buffer_length = length;
+    printf("Creating buffer of length = %d\n", buffer_length);
+
+    int *buffer = malloc(sizeof(int) * buffer_length);
+    int buffer_location = 0;
+
+
+    /* Create first comparator */
+    // Maybe this should be after number generation but i think it's better to have the thread running and then "send" a number
+    int i = 420;
+    pthread_create(&threads[1], NULL, test_function, i);
+
+    /*Generate nubmers and send?*/
+    for (size_t i = 0; i < buffer_length; i++)
+    {
+        buffer[buffer_location] = rand();
+        buffer_location = ++buffer_location % buffer_length;
+    }
+
+    /* Generate ends */
+    for (size_t i = 0; i < buffer_length; i++)
+    {
+        buffer[buffer_location] = -1;
+        buffer_location = ++buffer_location % buffer_length;
+    }
+
+
+
+
+
+}
 
 int main(int argc, char *argv[]){
     int c;
@@ -18,7 +65,7 @@ int main(int argc, char *argv[]){
 
 
     /* Read command-line options. */
-    while((c = getopt(argc, argv, "l:s:")) != -1) {
+    while((c = getopt(argc, argv, "b:l:s:")) != -1) {
         switch(c) {
             case 'l':
                 length = atol(optarg);
@@ -26,25 +73,49 @@ int main(int argc, char *argv[]){
             case 's':
                 seed = atoi(optarg);
                 break;
+            case 'b':
+                buffer_size = atoi(optarg);
             case '?':
+            if (optopt == 'b' ||
+                optopt == 'l' ||
+                optopt == 's')
+            {
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            }
+            else if (isprint(optopt))
+            {
+                fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+            }
+            else
+            {
                 fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
-                return -1;
-            default:
-                return -1;
+            }
+            return -1;
+        default:
+            return -1;
         }
     }
 
     /* Seed such that we can always reproduce the same random vector */
     srand(seed);
 
+    /*Create array of threads */
+    threads = malloc(sizeof(pthread_t) * length);
+
     clock_gettime(CLOCK_MONOTONIC, &before);
-    /* Do your thing here */
+
+    pthread_create(&threads[0], NULL, generate_numbers, length);
+
+
+    /* Wait for first thread to finish */
+    pthread_join(threads[0], NULL);
 
 
 
 
 
-    /* Do your thing here */
+
+
     clock_gettime(CLOCK_MONOTONIC, &after);
     double time = (double)(after.tv_sec - before.tv_sec) +
                   (double)(after.tv_nsec - before.tv_nsec) / 1e9;
